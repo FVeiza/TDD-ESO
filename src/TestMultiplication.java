d56fccdbd0ced0b1e929fe5ff879fbd034ba89f1
FVeiza <fegmve@gmail.com>
2022-09-21 10:31:05 -0300

    Commit 1.1

--- src/TestMultiplication.java
+++ src/TestMultiplication.java
@@ -6,8 +6,8 @@ import org.junit.Test;
 public class TestMultiplication {
 	@Test
 	 public void testMultiplication() {
-	       Dollar five = new Dollar(5);
-	       five.times(2);
-	       assertEquals(10, five.amount);
+		   Dollar five = new Dollar(5);
+		   assertEquals(new Dollar(10), five.times(2));
+		   assertEquals(new Dollar(15), five.times(3));
 	 }
 }
