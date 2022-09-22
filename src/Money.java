b943d815dc9f2154d5dc0fa44eb75ad95edb4643
FVeiza <fegmve@gmail.com>
2022-09-21 22:13:34 -0300

    Commit 4

--- src/Money.java
+++ src/Money.java
@@ -1,18 +1,30 @@
 abstract class Money  {
+
    protected int amount;
+   
+   private String currency;
+   
+   Money(int amount, String currency) {
+	      this.amount = amount;
+	      this.currency = currency;
+   }
+   
+   static Money dollar(int amount)  {
+	      return new Dollar(amount, "USD");
+   }
+
+   static Money franc(int amount) {
+	      return new Franc(amount, "CHF");
+   }
    
    public boolean equals(Object object) {
 	      Money money = (Money) object;
 	      return amount == money.amount && getClass().equals(money.getClass());
-   } 
-   
-   static Money dollar(int amount)  {
-	      return new Dollar(amount);
-   }
-   
-   static Money franc(int amount) {
-	      return new Franc(amount);
    }
    
    abstract Money times(int multiplier);
+   
+   String currency() {
+	      return currency;
+   }
 }
