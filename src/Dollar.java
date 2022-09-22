846a0a7a2a40e213190177733b24362028bb2602
FVeiza <fegmve@gmail.com>
2022-09-21 21:46:14 -0300

    Commit 2

--- src/Dollar.java
+++ src/Dollar.java
@@ -1,12 +1,10 @@
 
-class Dollar {
-	   private int amount;
-	   Dollar(int amount) {
+class Dollar extends Money{
+	Dollar(int amount) {
 	      this.amount= amount;
 	   }
 	   Dollar times(int multiplier) {
 		  return new Dollar(amount * multiplier);
-	   }
-	   
+	}	   
  }	
 
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
 	public static void main(String args[]) {
 		TestMultiplication t = new TestMultiplication();
 		t.testMultiplication();
+		t.testEquality();
+		t.testFrancMultiplication();
 	}
 }
